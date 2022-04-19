f110619f48544043ef8784d9e96d83a8545739e9
faisalknoldus <mohd.faisal@knoldus.com>
2022-04-19 18:23:27 +0530

    Initial commit

---
+++ src/main/java/com/knoldus/kup/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.knoldus.kup.repository;
+
+import com.knoldus.kup.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
